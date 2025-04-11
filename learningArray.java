3d5aee67a0066da702276084ba797e6758654824
DeveloperManikDas <developermanikdas@gmail.com>
2025-04-11 21:22:13 +0530

    add make pair of array elements

--- learningArray.java
+++ learningArray.java
@@ -47,14 +47,22 @@ public class learningArray {
         return -1;
     }
 
+    public static void makePair(int arr[]) {
+        for (int i = 0; i < (arr.length - 1); i++) {
+            for (int j =i+1; j < arr.length; j++) {
+                System.out.print(" (" + arr[i] + " " + arr[j] + ") ");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+        int arr[] = { 1, 2, 3, 4, 5 };
         // System.out.println(getLargest(arr));
         // System.out.println(binarySearch(arr, 100));
-        reverseArray(arr);
-        for(int num : arr){
-            System.out.println((num));
-        }
-
+        // reverseArray(arr);
+        // for(int num : arr){
+        // System.out.println((num));
+        // }
+        makePair(arr);
     }
 }
